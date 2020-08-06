66b5e5f0f6dc4f8d32e50eb63968d3e5ba3ccba7
tanvirsp <tanvirgraphic@gmail.com>
2020-08-06 18:23:36 +0600

    practise

---
+++ letconst.js
@@ -0,0 +1,13 @@
+const hubby ="Elias Kanchon";
+//hubby ="Omar Sunny";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+//numbers = ["Sunny", "Khan", "Malek"];
+console.log(numbers);
+
+
+let patientName = "Rahim Chacha";
+console.log(patientName);
---
+++ template.js
@@ -0,0 +1,12 @@
+const firstName = "Justin";
+const lastName = "TiberLake";
+
+const fullName = firstName + " " + lastName;
+
+const fullName2 = `${firstName} ${lastName} ${20+30+50} is a good boy`;
+
+const multiLine = `I love  your
+I miss you
+no I don't need you.`;
+
+console.log(multiLine);
