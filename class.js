953f9e9e7c2fc85c51d34746db3d652f9e0783e2
tanvirsp <tanvirgraphic@gmail.com>
2021-01-25 06:15:20 +0600

    es6 complete

---
+++ class.js
@@ -0,0 +1,11 @@
+class Student{
+    constructor(id, firstName, lastName, roll){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roll = roll;
+    }
+}
+
+
+const student1 = new Student(01,"Reza", "Quraishi", 12);
